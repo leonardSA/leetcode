5bd5e073a55ee86097a5d6323698252d300eb8d4
leonardSA <leonard.stephenauguste@gmail.com>
2020-12-03 15:09:54 +0100

    prob100: add solution 1 in c

---
+++ 100.SameTree/c/1s_sameTree.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+        return true;
+    else if ((p == NULL) ^ (q == NULL))
+        return false;
+    else if (p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+int main(void) {
+    return 0;
+}
